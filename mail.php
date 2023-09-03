136ea32541451c317723cf94ad12e9bb63e40616
Eugenia Herrera <eugenia.herrera@wilko.com>
2023-09-03 22:49:09 +0100

    Add all files

---
+++ mail.php
@@ -0,0 +1,12 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+$formcontent=" From: $name \n Email: $email \n Subject: $subject \n Message: $message";
+$recipient = "eu.herrera@gmail.com";
+$subject = "Portfolio Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+?>
